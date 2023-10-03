63c2b3cdab2638aea8ff7c67a8c93d233a821490
yevhenii ushakov <y.ushakov@seedium.io>
2023-10-04 01:57:29 +0300

    feat(): add config

---
+++ config/build/buildResolvers.ts
@@ -0,0 +1,8 @@
+// types
+import type { ResolveOptions } from "webpack";
+
+export const buildResolvers = (): ResolveOptions => {
+  return {
+    extensions: [".tsx", ".ts", ".js"],
+  };
+};
---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,31 @@
+import webpack from "webpack";
+// utils
+import { buildPlugins } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
+import { buildDevServer } from "./buildDevServer";
+import { buildResolvers } from "./buildResolvers";
+// types
+import type { BuildOptions } from "./types/config";
+
+export const buildWebpackConfig = (
+  options: BuildOptions
+): webpack.Configuration => {
+  const { paths, mode, isDev } = options;
+
+  return {
+    mode: mode,
+    entry: paths.entry,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(options),
+    },
+    resolve: buildResolvers(),
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+  };
+};
